3f87bd755667d0599d1f5ab999d1ce816eec35ef
Manon de Saint-Ferreol <manon2sf@gmail.com>
2020-08-18 15:56:49 +0200

    Updates

---
+++ precache-manifest.664de7a33abf5178777a4f80aed1e30d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3eb32c3ff8ddfdfc4157e9ef2cf5174",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "e33865cb05ff1865aee8",
+    "url": "/portfolio-react/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "5a666dca0801f744b41f",
+    "url": "/portfolio-react/static/css/main.bbb1226d.chunk.css"
+  },
+  {
+    "revision": "e33865cb05ff1865aee8",
+    "url": "/portfolio-react/static/js/2.b5a37b10.chunk.js"
+  },
+  {
+    "revision": "2ecd760a01b9b08172f3fbed44ec5739",
+    "url": "/portfolio-react/static/js/2.b5a37b10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a666dca0801f744b41f",
+    "url": "/portfolio-react/static/js/main.ec38c32f.chunk.js"
+  },
+  {
+    "revision": "feb6fec8f95565f3aa5d",
+    "url": "/portfolio-react/static/js/runtime-main.c94dc782.js"
+  },
+  {
+    "revision": "908158a8283e52c14b8202f721cd38a9",
+    "url": "/portfolio-react/static/media/CV-Manon-de-Saint-Ferreol.908158a8.pdf"
+  },
+  {
+    "revision": "9ab214aa7343de7b674b3deac082e726",
+    "url": "/portfolio-react/static/media/github-square-brands.9ab214aa.svg"
+  },
+  {
+    "revision": "4c7b298563958c8227fd152774c21af1",
+    "url": "/portfolio-react/static/media/linkedin-brands.4c7b2985.svg"
+  },
+  {
+    "revision": "9d2080cbf3a81b3fd31981bd1ae9c599",
+    "url": "/portfolio-react/static/media/waves.9d2080cb.svg"
+  }
+]);
